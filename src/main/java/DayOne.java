ed5ec73f319206a972145ef32b0b68230e0b1ede
Blazej Bucko <blazej.bucko@gmail.com>
2019-08-14 23:57:22 +0200

    DayOne - part I completed

--- src/main/java/DayOne.java
+++ src/main/java/DayOne.java
@@ -1,2 +1,22 @@
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class DayOne {
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        var uri = DayOne.class.getResource("dayOne.txt");
+        var lines = Files.readAllLines(Path.of(uri.toURI()));
+        System.out.println(new DayOne().process(lines));
+    }
+
+    int process(List<String> inputs) {
+        return inputs
+                .stream()
+                .map(Integer::parseInt)
+                .reduce(Integer::sum).orElse(-1);
+    }
+
 }
--- src/test/java/DayOneTest.java
+++ src/test/java/DayOneTest.java
@@ -1,13 +1,26 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DayOneTest {
 
+    private final DayOne dayOne = new DayOne();
+
     @Test
-    public void testSomething() {
-        assertTrue(true);
+    void testPlusOnes() {
+        assertEquals(3, dayOne.process(List.of("+1", "+1", "+1")));
     }
 
+    @Test
+    void testPlusOneAndMinusTwo() {
+        assertEquals(0, dayOne.process(List.of("+1", "+1", "-2")));
+    }
+
+    @Test
+    void testMinusOneTwoThree() {
+        assertEquals(-6, dayOne.process(List.of("-1", "-2", "-3")));
+    }
 
 }
